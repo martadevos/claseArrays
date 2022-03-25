8a2e985589ece56b6c203d7c0bedc93007e0a1ae
martadevos <marta.devos@iesnervion.es>
2022-03-25 10:57:57 +0100

    .

--- src/FuncionesArrays/Funciones.java
+++ src/FuncionesArrays/Funciones.java
@@ -5,33 +5,67 @@ import java.util.Arrays;
 public class Funciones {
     public static int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     public static int[] array2 = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    public static int[] array3 = {1, 2, 3, 4, 6, 7, 8, 9, 10};
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(array1));/*Arrays.tostring(nombreArray) muestra los valores
-                                                     del array entre corchetes*/
-        System.out.println(Arrays.equals(array1, array2));/*Arrays.equals(nombreArray1, nombreArray2) muestra
-                                                           si dos arrays son iguales (develve true si lo son
-                                                           y false si no)*/
-        System.out.println(Arrays.binarySearch(array1, 5));/*Arrays.equals(nombreArray, valorABuscar) busca
-                                                                un valor dentro de un array, si lo encuentra
-                                                                devuelve su posición*/
-         System.out.println(Arrays.binarySearch(array1, 50));/*si no lo encuentra, muestra un valor negativo
-                                                                  (el valor que ocuparía si se insertara
-                                                                  en positivo y -1)*/
-        System.out.println(Arrays.binarySearch(array1, 1, 5, 2));/*Se puede hacer una
-                                                                  búsqueda en un rango de posiciones del array
-                                                                  con Arrays.binarySearch(nombreArray,
-                                                                  inicioDelRango, finDelRango, valorABuscar)*/
-        //Para insertar un valor nuevo a un array desordenado, insertarlo al final si cabe; para uno ordenado:
-        int[] array4 = Arrays.copyOf(array3, array3.length+1);
-        System.out.println(array4.length);
-        if (array4.length - 1 - (-Arrays.binarySearch(array4, 5) - 1) >= 0) {
-            System.arraycopy(array4, -Arrays.binarySearch(array4, 5) - 1, array4, -Arrays.binarySearch(array4, 5), array4.length - 1 - (-Arrays.binarySearch(array4, 5) - 1));
-        }
-        array4[-Arrays.binarySearch(array4, 5) - 1] = 5;
-        System.out.println(Arrays.toString(array4));
 
-for (int i = array4.length-1; i>-Arrays.binarySearch(array4, 5) - 1)
+
+    public static void main(String[] args) {
+
+        //Arrays.fill(nombreArray, valor) rellena un array
+        int[] array = new int[5];
+        Arrays.fill(array, 2);
+        System.out.println(Arrays.toString(array));
+
+        //Arrays.tostring(nombreArray) muestra los valores del array entre corchetes
+        int[] arraytoString = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(Arrays.toString(arraytoString));
+
+        //Arrays.equals(nombreArray1, nombreArray2) develve true si son iguales y false si no*/
+        int[] array1equals = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] array2equals = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        System.out.println(Arrays.equals(array1equals, array2equals));
+
+       //Arrays.binarySearch(nombreArray, valorABuscar) busca un valor dentro de un array
+        int[] arrayBinarySearch = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(Arrays.binarySearch(arrayBinarySearch, 5));/*Si encuentra el valor
+                                                                devuelve su posición*/
+
+        System.out.println(Arrays.binarySearch(arrayBinarySearch, 50));/*si no, muestra el lugar
+                                                                que ocuparía -1 en negativo*/
+
+        System.out.println(Arrays.binarySearch(arrayBinarySearch, 1, 5, 2));/*Poniendo
+                                                                un rango podemos vuscar el valor
+                                                                solo en un tramo del array*/
+
+        //Arrays.copyOf(nombreArrayACopiar, longitudDeLaCopia) copia un array en otro la longitud que elijamos
+        int[] arrayCopyOf = Arrays.copyOf(arrayBinarySearch, 3);
+        System.out.println(Arrays.toString(array));
+
+        //Para insertar un valor nuevo a un array desordenado, insertarlo al final si cabe; para uno ordenado:
+        int[] arrayInsertar = {1, 2, 3, 4, 6, 7, 8, 9, 10};
+        System.out.println(arrayInsertar.length);
+        int i = (-Arrays.binarySearch(arrayInsertar, 5) - 1);
+        if (arrayInsertar.length - 1 - i >= 0) {
+            System.arraycopy(arrayInsertar, i,
+                    arrayInsertar, i + 1,
+                    arrayInsertar.length - 1 - i);
+        }
+        arrayInsertar[-Arrays.binarySearch(arrayInsertar, 5) - 1] = 5;
+        System.out.println(Arrays.toString(arrayInsertar));
+
+        //ordenar un array desordenado
+        int[] arrayOrdenar= {1, 3, 2, 9, -10, 6};
+        Arrays.sort(arrayOrdenar);
+        System.out.println(Arrays.toString(arrayOrdenar));
+
+        //Borrar un elemento de un array ordenado
+        int x = Arrays.binarySearch(arrayOrdenar, -10);
+        System.out.println(x);
+        if (arrayOrdenar.length - 1 - x >= 0) {
+            System.arraycopy(arrayOrdenar, x + 1,
+                    arrayOrdenar, x,
+                    arrayOrdenar.length - 1 - x);
+        }
+        int[] arrayBorrar = Arrays.copyOf(arrayOrdenar, arrayOrdenar.length-1);
+        System.out.println(Arrays.toString(arrayBorrar));
 
     }
 }
